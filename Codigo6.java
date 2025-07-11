23a446d4de402faba330034e77c899a90918f611
Raul Maldonado <r.maldonadop95@gmail.com>
2025-07-11 11:18:21 -0600

    revision 6

--- Codigo6.java
+++ Codigo6.java
@@ -1,24 +1,31 @@
-public class codigo6 {
-	  
-	    int[] n = int[20];
+import java.util.Scanner;
 
-	    for (int i = 0; i < 20; i+) {
-	      n[i] = (int)(Math.random() * 381) + 20;
-	      System.print(n[i] + " ");
-	    }
-	    
-	    System.out.printl("\n¿Qué números quiere resaltar? ");
-	    System.out.print("(1 – los múltiplos de 5, 2 – los múltiplos de 7): ");
-	    int opcion = Integer.parseInt(System.console().readLine();
+public class Codigo6 {
 
-	    int multiplo = (opcion == 1) : 5 ? 7;
+	public static void main(String[] args) {
+		int[] n = new int[20];
 
-	    foreach (char e : n) {
-	      if (e % multiplo == 0) {
-	        System.out.print("[" + e + "] ");
-	       else {
-	        System.in.print(e + " ");
-	      }
-	    }
-	  
+		for (int i = 0; i < 20; i++) {
+			n[i] = (int)(Math.random() * 381) + 20;
+			System.out.print(n[i] + " ");
+		}
+
+		System.out.println("\n¿Qué números quiere resaltar?");
+		System.out.print("(1 – los múltiplos de 5, 2 – los múltiplos de 7): ");
+
+		Scanner scanner = new Scanner(System.in);
+		int opcion = Integer.parseInt(scanner.nextLine());
+
+		int multiplo = (opcion == 1) ? 5 : 7;
+
+		for (int e : n) {
+			if (e % multiplo == 0) {
+				System.out.print("[" + e + "] ");
+			} else {
+				System.out.print(e + " ");
+			}
+		}
+
+		scanner.close();
 	}
+}
